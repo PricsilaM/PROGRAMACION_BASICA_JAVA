c26031f7d895079847cae16fa3935127a030071a
PricsilaM <1538146@senati.pe>
2024-04-23 13:46:27 -0500

    Operadores Logicos

---
+++ OperadoresLogicoss/src/operadoreslogicoss/OperadoresLogicoss.java
@@ -0,0 +1,22 @@
+
+package operadoreslogicoss;
+
+public class OperadoresLogicoss {
+
+    public static void main(String[] args) {
+        boolean esVerdadero = true;
+        boolean esFalso = false;
+        //Operador AND (&&)
+        boolean resultadoAnd = esVerdadero && esFalso;
+                System.out.println("AND: "+ resultadoAnd);
+                
+        //
+        boolean resultadoO = esVerdadero || es Falso;
+                System.out.println("OR: "+ resultadoO);
+        
+        //Operador NOT (!)
+        boolean resultadoNot =!esVedadero;
+                System.out.println("NOT: "+ resultadoNot);
+    }
+    
+}
